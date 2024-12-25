4bc1756eb555379a1bf516e324fde7ae30f47761
sooonotkota <dakotart08@icloud.com>
2024-12-25 17:02:19 -0600

    adding basic javascript
    Adding function call to begin Api

--- Kotas.js
+++ Kotas.js
@@ -1,11 +1,13 @@
-let apiKey = "c5fb501012566e9bd47a3bo39e25tb37";
-let context = "Please tell us a french poem.";
-let prompt = "Please tell me a french poem.";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+function generatePoem(event) {
+  event.preventDefault();
 
-axios.get(apiUrl).then(displayAnswer);
+  new Typewriter("poem", {
+    strings: "Please wait... generating a poem!",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
 
-new Typewriter("#h1", {
-  strings: "Please wait... generating a poem!",
-  autoStart: true,
-});
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
