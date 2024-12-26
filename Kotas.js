cc293b7b9d8ca16a3c58420025abe827842ab04e
sooonotkota <dakotart08@icloud.com>
2024-12-26 10:11:47 -0600

    intregrating my api
    Adding the API to add to the poem generator.

--- Kotas.js
+++ Kotas.js
@@ -1,12 +1,24 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Please wait... generating a poem!",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "c5fb501012566e9bd47a3bo39e25tb37";
+  let prompt = 'User instructions: Generate a poem about ${instructionsInput.value}'; 
+  let context = "You are a romantic poem expert, and love to write short poems. Your mission is to generate a 4 line poem in basic HTML. Make sure to follow the user instructions.";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
